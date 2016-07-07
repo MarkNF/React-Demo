560feb6b0604dd72889e1841cb190c53e4f32be1
MarkNF <mflorian2515@gmail.com>
2016-07-06 22:14:28 -0400

    Finished React Router Transition Animation (100%)
    Installed style loader and css transition group
    Used React.cloneElement to pass keys to props.children

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,16 @@
 var React = require('react');
+var ReactCssTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 function Main (props) {
 	return (
 			<div className='main-container'>
-				{props.children}
+				<ReactCssTransitionGroup
+					transitionName="appear"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}>
+						{React.cloneElement(props.children, {key: props.location.pathname})}
+				</ReactCssTransitionGroup>
 			</div>
 		)
 };
